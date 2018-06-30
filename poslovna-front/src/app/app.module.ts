e0eb56ab4095dcec3c4c6670c6bf6a95ff7bf0db
Marko Curuvija <pryda_95@yahoo.com>
2018-06-30 19:09:34 +0200

    ispravke

--- poslovna-front/src/app/app.module.ts
+++ poslovna-front/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { KlijentComponent } from './klijent/klijent.component';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -6,10 +7,6 @@ import { AppRoutingModule } from './/app-routing.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { DrzaveComponent } from './drzave/drzave.component';
 import { NaseljenaMestaComponent } from './naseljena-mesta/naseljena-mesta.component';
-<<<<<<< HEAD
-import { KlijentComponent } from './klijent/klijent.component';
-import { FormsModule } from '@angular/forms';
-=======
 import { DrzaveEditComponent } from './drzave/drzave-edit/drzave-edit.component';
 import { NaseljenaMestaEditComponent } from './naseljena-mesta/naseljena-mesta-edit/naseljena-mesta-edit.component';
 import { DrzaveService } from './services/drzave.service';
@@ -17,7 +14,6 @@ import { NaseljenaMestaService } from './services/naseljena-mesta.service';
 import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
->>>>>>> 8d8b704862537160a40c143d6bc4e122e25c59a4
 
 @NgModule({
   declarations: [
@@ -25,27 +21,19 @@ import { FormsModule } from '@angular/forms';
     NavigationComponent,
     DrzaveComponent,
     NaseljenaMestaComponent,
-<<<<<<< HEAD
-    KlijentComponent
-=======
     DrzaveEditComponent,
-    NaseljenaMestaEditComponent
->>>>>>> 8d8b704862537160a40c143d6bc4e122e25c59a4
+    NaseljenaMestaEditComponent,
+    KlijentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    FormsModule
-=======
     HttpClientModule,
     FormsModule,
-    
   ],
   providers: [
     DrzaveService,
     NaseljenaMestaService,
->>>>>>> 8d8b704862537160a40c143d6bc4e122e25c59a4
   ],
   bootstrap: [AppComponent]
 })

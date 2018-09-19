b72f30c9e2b0043e7f9080b3c6fb9d0c2ddb1210
Stefan Lukic <stefan.lukic@yahoo.com>
2018-09-19 18:20:10 +0200

    uplatnice za uplatu i isplatu

--- poslovna-front/src/app/nalog-za-isplatu/nalog-za-isplatu.component.ts
+++ poslovna-front/src/app/nalog-za-isplatu/nalog-za-isplatu.component.ts
@@ -1,6 +1,13 @@
 import { AnalitikaService } from './../services/analitika.service';
 import { AnalitikaIzvoda } from './../modeli/analitika-izvoda';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { NovaIsplata } from '../modeli/nova-isplata';
+import { Valuta } from '../modeli/valuta';
+import { RacunLica } from '../modeli/racunLica';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
+import { Location } from '@angular/common';
+import { ValuteService } from '../services/valute.service';
+import { RacunLicaService } from '../services/racun-lica.service';
 
 @Component({
   selector: 'app-nalog-za-isplatu',
@@ -8,14 +15,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nalog-za-isplatu.component.css']
 })
 export class NalogZaIsplatuComponent implements OnInit {
-
+  valutaShow: Valuta;
   analitikaIzvoda: AnalitikaIzvoda;
   file: any;
+  datum1: Date = new Date('MM/dd/yyyy');
+  model: any = {};
+  date1 = new Date(this.model.datum_otvaranja);
+  nalogZaIsplatu: NovaIsplata = new NovaIsplata("","","",null,null,0,"",this.date1,0);
+  valuta: Valuta;
+  racun_duznika: RacunLica;
+  valute: Valuta[];
+  racuni: RacunLica[];
+  racunId: string;
 
-  constructor(private analitikaService: AnalitikaService) { }
+  constructor(private analitikaService: AnalitikaService,
+              private location: Location,
+              private valuteService: ValuteService,
+              private racuniService: RacunLicaService) { }
 
-  ngOnInit() {
-  }
 
   onChange(event) {
     this.file = event.srcElement.files;
@@ -25,5 +42,41 @@ export class NalogZaIsplatuComponent implements OnInit {
   upload() {
     this.analitikaService.loadFromFile(this.file).subscribe();
   }
+
+  noviNalogIsplataSubmit(forma: NgForm){
+    this.nalogZaIsplatu.duznik = forma.value.duznik;
+    this.nalogZaIsplatu.iznos = forma.value.iznos;
+    this.nalogZaIsplatu.datum_prijema = forma.value.datum_prijema;
+    this.nalogZaIsplatu.model_odobrenja = forma.value.model_odobrenja;
+    this.nalogZaIsplatu.poverilac_primalac = forma.value.poverilac_primalac;
+    this.nalogZaIsplatu.poziv_na_brZ = forma.value.poziv_na_brZ;
+    this.nalogZaIsplatu.svrha_placanja = forma.value.svrha_placanja;
+    this.nalogZaIsplatu.valuta = this.valuta;
+    this.getRacun();
+    this.nalogZaIsplatu.racun_duznika = this.racun_duznika;
+
+    this.analitikaService.insertNalogZaIsplatu(this.nalogZaIsplatu).subscribe();
+    forma.reset();
+    this.location.back();
+
+}
+getRacuniLica(): void{
+  this.racuniService.getRacuni().subscribe(racuni => this.racuni = racuni);
+}
+getValute(): void {
+  this.valuteService.getValute().subscribe(valute => (this.valute = valute));
+}
+
+
+getRacun(){
+  this.racuniService.getRacun(this.racunId).subscribe((racun) => this.racun_duznika = racun);
+}
+
+ngOnInit() {
+  this.getValute();
+  this.getRacuniLica();
+
+}
+
 
 }
--- poslovna-front/src/app/services/analitika.service.ts
+++ poslovna-front/src/app/services/analitika.service.ts
@@ -6,6 +6,11 @@ import {
   HttpRequest
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NovaUplata } from '../modeli/nova-uplata';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+import { NovaIsplata } from '../modeli/nova-isplata';
+
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-type': 'application/json' })
@@ -15,7 +20,9 @@ const httpOptions = {
 export class AnalitikaService {
   private url_analitikaFile = 'http://localhost:8080/analitikaFile';
   private url_analitikaFileUplata ='http://localhost:8080/analitikaFileUplata';
-
+  private urlUplata = 'http://localhost:8080/analitikaUplata';
+  private urlIsplata = 'http://localhost:8080/analitikaIsplata';
+  
   constructor(private http: HttpClient) {}
 
   loadFromFile(file: File): Observable<HttpEvent<{}>> {
@@ -44,5 +51,28 @@ export class AnalitikaService {
     });
 
     return this.http.request(req);
+  }
+
+  insertNalogZaUplatu(nalogzaUplatu: NovaUplata): Observable<NovaUplata> {
+    return this.http.post<NovaUplata>(this.urlUplata, nalogzaUplatu, httpOptions).pipe(
+      catchError(this.handleError<NovaUplata>('insertNalogZaUplatu'))
+    );
+  }
+
+  insertNalogZaIsplatu(nalogZaIsplatu: NovaIsplata): Observable<NovaIsplata> {
+    return this.http.post<NovaIsplata>(this.urlIsplata, nalogZaIsplatu, httpOptions).pipe(
+      catchError(this.handleError<NovaIsplata>('insertNalogZaIsplatu'))
+    );
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
